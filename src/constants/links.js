176e575de19122c343f23630f274f47bad76812e
smhaley <shawnh87@gmail.com>
2025-01-17 06:06:53 -0600

    update link (#6)

--- src/constants/links.js
+++ src/constants/links.js
@@ -1,6 +1,4 @@
 export const GITHUB =  "https://github.com/smhaley/"
 export const LINKEDIN = "https://www.linkedin.com/in/shawn-haley/"
-export const RESUME =
-  "https://docs.google.com/document/d/1Rr-T2pQ-8Xd39VyT3fBhqCLBSFoJIZXOBiuZuCwxD6E/preview";
-
+export const RESUME ="https://docs.google.com/document/d/1F0kWrm7Lvmn6FKTMSyMVDiO8S54X8H9SQWUwSRjoQik/preview"
 export const SITELINK = "https://github.com/smhaley/smhaley_portfolio";
